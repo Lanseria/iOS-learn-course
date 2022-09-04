9465c0f999ccbc4c29302c0f98f0b811306901cc
lanseria <zhangchao564265135@hotmail.com>
2022-09-04 20:56:22 +0800

    Initial Commit

---
+++ FlcMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlcMap
+//
+//  Created by 张超 on 2022/9/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlcMap/FlcMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlcMapApp.swift
+//  FlcMap
+//
+//  Created by 张超 on 2022/9/4.
+//
+
+import SwiftUI
+
+@main
+struct FlcMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
